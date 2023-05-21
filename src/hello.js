0eb8fbde3beb142d4bcc42373ba8eacb76c4bc4a
ravikant1983 <111572726+ravikant1983@users.noreply.github.com>
2023-05-21 18:31:24 +0530

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -5,7 +5,7 @@ const hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Hello From Ravikant G! CICD",
+        message: "Hello From Ravikant G! CICD new",
       },
       null,
       2
